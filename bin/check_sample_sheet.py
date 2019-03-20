efe3ba1e03f80664f564fc0cf29bf0a6d55a78ae
Hannah Pliner <hpliner@gmail.com>
2019-03-20 16:01:53 -0700

    bug from no header

--- bin/check_sample_sheet.py
+++ bin/check_sample_sheet.py
@@ -33,12 +33,13 @@ if __name__ == '__main__':
     # check for header
     topline_orig = sheet.readline()
     topline = topline_orig.strip().split(",")
+    line_num = 1
     if topline[0] == 'RT Barcode' and topline[1] =='Sample ID' and topline[2] == 'Reference Genome':
         sample_out = topline_orig
         header = True
     else:
         sample_out = 'RT Barcode,Sample ID,Reference Genome\\n'
-        check_line(topline_orig)
+        check_line(topline_orig, line_num)
         sample_out = sample_out + topline_orig
 
     # Check RT and Genomes against possible
