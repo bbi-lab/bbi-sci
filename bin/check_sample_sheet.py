931bd95ed3589951bd2ae0196d25d10103c2afad
Hannah Pliner <hpliner@gmail.com>
2019-04-26 15:36:04 -0700

    fix extra columns issue

--- bin/check_sample_sheet.py
+++ bin/check_sample_sheet.py
@@ -37,14 +37,23 @@ if __name__ == '__main__':
     topline_orig = sheet.readline()
     topline = topline_orig.strip().split(",")
     line_num = 1
-    if topline[0] == 'RT Barcode' and topline[1] =='Sample ID' and topline[2] == 'Reference Genome':
-        sample_out = topline_orig + '\n'
-        header = True
+    num_cols = len(topline)
+    if num_cols == 3:
+        if topline[0] == 'RT Barcode' and topline[1] =='Sample ID' and topline[2] == 'Reference Genome':
+            sample_out = 'RT Barcode,Sample ID,Reference Genome\n'
+        else:
+            sample_out = 'RT Barcode,Sample ID,Reference Genome\n'
+            check_line(topline_orig, line_num)
+            sample_out = sample_out + topline_orig
     else:
-        sample_out = 'RT Barcode,Sample ID,Reference Genome\n'
-        check_line(topline_orig, line_num)
-        sample_out = sample_out + topline_orig
-
+        if topline[0] == 'RT Barcode' and topline[1] =='Sample ID' and topline[2] == 'Reference Genome':
+            sample_out = topline_orig + "\n"
+        else:
+            sample_out = 'RT Barcode,Sample ID,Reference Genome'
+            for i in range(3, len(topline)):
+                sample_out = sample_out + ",Column" + str(i)
+            check_line(topline_orig, line_num)
+            sample_out = sample_out + "\n" +  topline_orig
     # Check RT and Genomes against possible
     error_count = 0
 
