fe59c91032e8a3c4eae727ae43475024934a19e3
Hannah Pliner <hpliner@gmail.com>
2019-10-23 03:58:11 -0700

    bug

--- bin/generate_exp_dash.R
+++ bin/generate_exp_dash.R
@@ -306,7 +306,7 @@ body <- tags$body(
                             <td>'), c1000, HTML('</td>
                           </tr>
                         </tbody>
-                      </table>')
+                      </table>'),
   barn,
    sent,
   HTML('                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
