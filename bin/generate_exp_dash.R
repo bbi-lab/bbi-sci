fff1c1e3e5d28c9331b5a237b769bbe5ca9bf5d9
Hannah Pliner <hpliner@gmail.com>
2019-10-23 04:00:46 -0700

    bug

--- bin/generate_exp_dash.R
+++ bin/generate_exp_dash.R
@@ -287,8 +287,8 @@ body <- tags$body(
             </ul>
           </div>
         </nav>
-       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" style="padding-top: 15px;">'),,
-   '<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" style="padding-top: 15px;">'),
+   HTML('<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
  <h1 class="h3" id="rt">Cell totals</h1>
 </div>
                     <table class="table table-hover">
