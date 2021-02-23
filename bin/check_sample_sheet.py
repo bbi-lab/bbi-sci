c21c44288cc6936ea9567cb62212be484ecd68a7
brgew <bge@uw.edu>
2021-02-22 16:40:25 -0800

    modifications for 2 level multi-experiment

--- bin/check_sample_sheet.py
+++ bin/check_sample_sheet.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import os
+import math
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 RT_FILE = os.path.join(SCRIPT_DIR, 'barcode_files/rt2.txt')
@@ -28,6 +29,7 @@ if __name__ == '__main__':
         rtfile = args.rt_barcode_file
         fix = 0
     rtdict = {}
+
     with open(rtfile) as rt_file:
         for line in enumerate(rt_file):
             line = line[1].strip().split("\t")
@@ -71,43 +73,81 @@ if __name__ == '__main__':
             line[0] = p1 + "-" + p2
             return ",".join(line)
 
+    def fix_line_exp(line, fix):
+        line = line.split(",")
+        line[1] = well_dict[(line[0],line[3])]
+        if fix == 0:
+            return ",".join(line)
+        if fix == 2:
+            p1 = line[0].split("-")[0]
+            p2 = line[0].split("-")[1]
+            p1 = p1[0:2] + "{0:0=2d}".format(int(p1[2:]))
+            p2 = p2[0:1] + "{0:0=2d}".format(int(p2[1:]))
+            line[0] = p1 + "-" + p2
+            return ",".join(line)
+        if fix == 3:
+            p1 = line[0].split("-")[0]
+            p2 = line[0].split("-")[1]
+            p1 = p1[0:1] + "{0:0=2d}".format(int(p1[1:]))
+            p2 = p2[0:1] + "{0:0=2d}".format(int(p2[1:]))
+            line[0] = p1 + "-" + p2
+            return ",".join(line)
+
     # precount samples per well
     sample_dict = dict()
     well_dict = dict()
+    with_exp = False
     with open(args.sample_sheet, 'r') as f:
         for line in f:
             line = line.split(",")
-            if line[1] in sample_dict:
-                sample_dict[line[1]].append(line[0])
+            if len(line) > 3:
+                if line[1] in sample_dict:
+                    sample_dict[line[1]].append((line[0],line[3]))
+                else:
+                    sample_dict[line[1]] = [(line[0],line[3])]
+                well_dict[(line[0],line[3])] = line[1]
+                with_exp = True
+            elif with_exp:
+                sys.stderr.write("One or more entries does not include Experiment label (third column) while others do.\n")
+                sys.exit(11)
             else:
-                sample_dict[line[1]] = [line[0]] 
-            well_dict[line[0]] = line[1]
+                if line[1] in sample_dict:
+                    sample_dict[line[1]].append(line[0])
+                else:
+                    sample_dict[line[1]] = [line[0]]
+                well_dict[line[0]] = line[1]
 
     for samp in sample_dict.keys():
         if len(sample_dict[samp]) > int(args.max_wells_per_sample):
+            div = len(sample_dict[samp])/math.ceil(len(sample_dict[samp])/int(args.max_wells_per_sample))
             group_count = 1
             curr_count = 1
             for well in sample_dict[samp]:
-                if curr_count <= int(args.max_wells_per_sample):
+                if curr_count <= div:
                     curr_count += 1  
                 else:
                     curr_count = 1
                     group_count += 1
                 well_dict[well] += "_fq_part" + str(group_count)
 
-    sheet = open(args.sample_sheet, 'r')
+    sheet = open(args.sample_sheet)
+
+    if with_exp:
+        fix_func = fix_line_exp
+    else:
+        fix_func = fix_line
+
     # check for header
     topline_orig = sheet.readline()
     topline = topline_orig.strip().split(",")
     line_num = 1
     num_cols = len(topline)
-    
     if num_cols == 3:
         if topline[1] =='Sample ID' and topline[2] == 'Reference Genome':
             sample_out = 'RT Barcode,Sample ID,Reference Genome\n'
         else:
             sample_out = 'RT Barcode,Sample ID,Reference Genome\n'
-            topline_orig = fix_line(topline_orig, fix)
+            topline_orig = fix_func(topline_orig, fix)
             check_line(topline_orig, line_num)
             sample_out = sample_out + topline_orig
     else:
@@ -117,7 +157,7 @@ if __name__ == '__main__':
             sample_out = 'RT Barcode,Sample ID,Reference Genome'
             for i in range(3, len(topline)):
                 sample_out = sample_out + ",Column" + str(i)
-            topline_orig = fix_line(topline_orig, fix)
+            topline_orig = fix_func(topline_orig, fix)
             check_line(topline_orig, line_num)
             sample_out = sample_out + "\n" +  topline_orig
     # Check RT and Genomes against possible
@@ -125,7 +165,7 @@ if __name__ == '__main__':
 
     line_num = 1
     for line in sheet:
-        line = fix_line(line, fix)
+        line = fix_func(line, fix)
         line_num += 1
         linesp = line.strip().split(",")
         if linesp[0] in (None, "") and linesp[1] in (None, "") and linesp[2] in (None, ""):
